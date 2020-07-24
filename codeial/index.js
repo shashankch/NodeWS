30eab3ee1d19c302db598011c4e6612aea9fa4ea
shashank singh chandel <shashakchandel@gmail.com>
2020-07-25 03:26:50 +0530

    added partials and layouts and styles

--- codeial/index.js
+++ codeial/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-
+const expressLayouts = require("express-ejs-layouts");
 // using express router
 
+app.use(express.static("./assets"));
+
+app.use(expressLayouts);
+
+// extract style and scripts from pages and put into head of layout.
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
 app.use("/", require("./routes"));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
