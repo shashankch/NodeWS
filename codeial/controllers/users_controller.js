61c0afad6b22827860de6a58cec57c3e2e600f52
shashank singh chandel <shashakchandel@gmail.com>
2020-08-04 00:21:18 +0530

    added sign-up

--- codeial/controllers/users_controller.js
+++ codeial/controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 module.exports.profile = function (req, res) {
   // res.end("<h1>user profile</h1>");
   return res.render("user_profile", {
@@ -20,12 +22,33 @@ module.exports.signIn = function (req, res) {
 };
 
 // get the sign up data
-module.exports.create=function (req,res) {
-  
-  // TODO
-}
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
 
-// sign in and create a session for the user 
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
+    }
+
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in finding user in signing up");
+          return;
+        }
+
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+// sign in and create a session for the user
 module.exports.createSession = function (req, res) {
   // TODO
 };
