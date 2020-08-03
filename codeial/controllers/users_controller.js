5002e81084ae9c921fd540a0a6715c7d58c095fd
shashank singh chandel <shashakchandel@gmail.com>
2020-08-03 23:48:09 +0530

    added sign-in and sign-up

--- codeial/controllers/users_controller.js
+++ codeial/controllers/users_controller.js
@@ -1,6 +1,31 @@
 module.exports.profile = function (req, res) {
   // res.end("<h1>user profile</h1>");
   return res.render("user_profile", {
-    title: "User profile"
+    title: "User profile",
   });
+};
+
+// render the sign up
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial| sign up",
+  });
+};
+
+// render sign in
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial| sign in",
+  });
+};
+
+// get the sign up data
+module.exports.create=function (req,res) {
+  
+  // TODO
+}
+
+// sign in and create a session for the user 
+module.exports.createSession = function (req, res) {
+  // TODO
 };
--- codeial/index.js
+++ codeial/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 // using express router
 const db = require("./config/mongoose");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.use(express.static("./assets"));
 
 app.use(expressLayouts);
