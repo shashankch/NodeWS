af5715f2d5ce1b696aa4ecdd7938fc6b48cf149a
shashank singh chandel <shashakchandel@gmail.com>
2020-08-06 02:01:15 +0530

    added passport authentication

--- codeial/controllers/users_controller.js
+++ codeial/controllers/users_controller.js
@@ -9,6 +9,13 @@ module.exports.profile = function (req, res) {
 
 // render the sign up
 module.exports.signUp = function (req, res) {
+  
+  
+  if (req.isAuthenticated()) {
+    return res.redirect('/users/profile');
+  }
+  
+  
   return res.render("user_sign_up", {
     title: "Codeial| sign up",
   });
@@ -16,6 +23,10 @@ module.exports.signUp = function (req, res) {
 
 // render sign in
 module.exports.signIn = function (req, res) {
+
+    if (req.isAuthenticated()) {
+     return  res.redirect("/users/profile");
+    }
   return res.render("user_sign_in", {
     title: "Codeial| sign in",
   });
@@ -50,5 +61,7 @@ module.exports.create = function (req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function (req, res) {
-  // TODO
+
+  return res.redirect('/');
+
 };
--- codeial/index.js
+++ codeial/index.js
@@ -5,6 +5,11 @@ const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 // using express router
 const db = require("./config/mongoose");
+// used for session cookie
+const session = require("express-session");
+
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,10 +23,29 @@ app.use(expressLayouts);
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
-app.use("/", require("./routes"));
-
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(
+  session({
+    name: "codeial",
+    // TODO change the secret before deployment in production mode
+    secret: "something",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
