76d5b207d7be5d326044387569d53b93841d59c1
shashank singh chandel <shashakchandel@gmail.com>
2020-08-05 22:29:17 +0530

    sign-out added

--- codeial/routes/users.js
+++ codeial/routes/users.js
@@ -8,7 +8,9 @@ router.get("/profile", usersController.profile);
 router.get("/sign-in", usersController.signIn);
 
 router.get("/sign-up", usersController.signUp);
+router.get("/sign-out", usersController.signOut);
 
 router.post("/create", usersController.create);
 
+router.post("/create-session", usersController.createSession);
 module.exports = router;
