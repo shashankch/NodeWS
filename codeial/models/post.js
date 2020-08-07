1aa03fd32b465f1bc6efd577db9e7c7ab852f4b7
shashank singh chandel <shashakchandel@gmail.com>
2020-08-08 01:35:42 +0530

    added comment schema

--- codeial/models/post.js
+++ codeial/models/post.js
@@ -11,6 +11,14 @@ const postSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+
+    // include the array of ids of all comments in this post Schema itself
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
   {
     timestamps: true,
